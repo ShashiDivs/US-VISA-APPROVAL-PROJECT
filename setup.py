c419e30fd690ba35a9f52de3a0584b475205c361
Gaajarla Sheshugoud <103266813+ShashiDivs@users.noreply.github.com>
2024-02-22 15:06:45 +0530

    Project Setup has been Created includes Template, Requirements and Setup files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="shashi",
+    author_email="shashipolity@gmail.com",
+    packages=find_packages()
+)
